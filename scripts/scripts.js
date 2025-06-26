9324f3dec3bcdc254b0f5c689ffccf5514d84ff8
Joaol324j <joaol324j@gmail.com>
2025-06-26 14:08:40 -0300

    fix: styles and scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,17 +2,15 @@ const botaoMostrarFormulario = document.getElementById("mostrar-formulario");
 const formulario = document.getElementById("formulario-compra");
 
 botaoMostrarFormulario.addEventListener("click", () => {
-	formulario.classList.toggle("ativo");
+    formulario.classList.toggle("ativo");
 });
 
-const saudacao = document.createElement("p");
 const hora = new Date().getHours();
 let mensagem = "";
 if (hora < 12) mensagem = "Bom dia";
 else if (hora < 18) mensagem = "Boa tarde";
 else mensagem = "Boa noite";
-saudacao.textContent = `${mensagem}, bem-vindo ao site do GTA V!`;
-document.querySelector(".informacoes").prepend(saudacao);
+alert(`${mensagem}, bem-vindo ao site do GTA V!`);
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
